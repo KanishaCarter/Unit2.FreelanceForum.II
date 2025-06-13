17438671c8304f23e07d2e549df1dc52f0bb63e6
KanishaCarter <kanishacarter90@outlook.com>
2025-06-13 00:23:19 -0500

    Created variables for DOM manipulation. Next step, is creating functions for average rate calculation and for displaying random freelancers using event listener.

--- index.js
+++ index.js
@@ -10,3 +10,41 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+const freelancerArray = [
+        {name:"Alice", occupation: "Writer", price: 40,},
+        {name:"Bob", occupation: "Teacher", price: 30},
+        {name:"Carol", occupation: "Programmer", price: 175},
+        {name:"Dave", occupations: "Designer", price: 100},
+        {name:"Eve", occupation: "Engineer", price: 200}
+]
+
+const body = document.querySelector("body");
+
+const container = document.createElement("div");
+const tableContainer = document.createElement("div");
+body.append(container);
+
+const h1 = document.createElement("h1");
+h1.textContent = "The Freelancer Forum";
+container.append(h1);
+container.append(tableContainer);
+
+
+const avgPriceH2 = document.createElement("h2");
+avgPriceH2.textContent = `The average rate is `;
+tableContainer.append(avgPriceH2);
+
+const table = document.createElement("table");
+tableContainer.append(table);
+
+const tableRow = document.createElement("tr");
+table.append(tableRow);
+
+const tableData = document.createElement("td");
+tableRow.append(tableData);
+
+function avgRate(freelancerArray) {
+    
+}
+
